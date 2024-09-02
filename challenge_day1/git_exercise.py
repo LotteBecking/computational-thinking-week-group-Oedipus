10e79662292d28d48bee65e090b4fafe66b02bfa
anacontrerase <ana.contreras.estrada@student.uva.nl>
2024-09-02 12:53:04 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,7 +1,2 @@
 hi = "This is Team Oedipus. We are:"
-from kimiya import *
-from lotte import *
-from ana import *
-from mana import *
-from thaiss import *
 print(hi, kimiya, lotte, thaiss, mana, ana)

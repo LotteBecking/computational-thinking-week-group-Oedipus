5b2f6661534dbddc6b9b98fad3ff495db990caf3
LotteBecking <lottebecking@gmail.com>
2024-09-02 12:52:51 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,2 +1,7 @@
 hi = "This is Team Oedipus. We are:"
+from kimiya import *
+from lotte import *
+from ana import *
+from mana import *
+from thaiss import *
 print(hi, kimiya, lotte, thaiss, mana, ana)

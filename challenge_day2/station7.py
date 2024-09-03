528dd4e1342af42d4616ccd78e7f7fdea5d51cfc
LotteBecking <lottebecking@gmail.com>
2024-09-03 12:09:44 +0200

    Update station7.py

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,7 +1,3 @@
-set1 = ("c+b+a*e", "d*b*e*c", 4.5)
-set2 = ("b*e*a+d", "a+e*d", 5.5)
-set3 = ("d+c", "d*c+e", 11)
-
 def solution_station_7(input):
     a = 3
     b = -1
@@ -9,5 +5,5 @@ def solution_station_7(input):
     d = 7
     e = 0.5
 
-    output = eval(input)
-    return output
+output = eval(input)
+return output

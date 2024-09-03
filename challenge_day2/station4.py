6e500bcbc41913e86e7b95d031bb0bfa713ace56
Kimiyaeltz <kimiya.grafin.zu.eltz@student.uva.nl>
2024-09-03 10:26:24 +0200

    station 3 and 4

---
+++ challenge_day2/station4.py
@@ -0,0 +1,17 @@
+def solution_station_3(number):
+    if number <= 1:
+        return "false"
+    if number <= 3:
+        return "true"
+    if number % 2 == 0 or number % 3 == 0:
+        return "false"
+    
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return "false"
+        i += 6
+    
+    return "true"
+
+print(solution_station_3(54)) 
